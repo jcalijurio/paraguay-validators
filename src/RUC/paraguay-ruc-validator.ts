5810c7462f7e0e0aa51b10c3cfa4e30c32035404
Jorge Calijurio <jorge.calijurio@gmail.com>
2020-04-27 16:27:42 -0300

    sequence identification

--- src/RUC/paraguay-ruc-validator.ts
+++ src/RUC/paraguay-ruc-validator.ts
@@ -17,9 +17,28 @@ export class ParaguayRUCValidator {
 
         const identityDigitsLength: number = adjustedRuc.length - 2;
         let valueSum = 0;
+        let increasingSequenceQtd = 0;
+        let decreasingSequenceQtd = 0;
+        let equalsDigitQtd = 0;
         for (let index = identityDigitsLength, multiplier = 2; index >= 0; index -= 1, multiplier += 1) {
             valueSum += Number(adjustedRuc[index]) * multiplier;
+
+            if (index > 0) {
+                const previousDigit = Number(adjustedRuc[index - 1]);
+                const currentDigit = Number(adjustedRuc[index]);
+                if (previousDigit === currentDigit)
+                    equalsDigitQtd += 1;
+
+                if (currentDigit > previousDigit)
+                    increasingSequenceQtd += 1;
+
+                if (previousDigit > currentDigit)
+                    decreasingSequenceQtd += 1;
+            }
         }
+
+        if (increasingSequenceQtd >= 6 || decreasingSequenceQtd >= 6 || equalsDigitQtd >= 6)
+            return false;
 
         const mod = valueSum % this._baseMod;
         const verifyDigit = mod > 1 ? this._baseMod - mod : 0;
--- tests/RUC/RUC-validation.spec.ts
+++ tests/RUC/RUC-validation.spec.ts
@@ -64,4 +64,18 @@ describe('RUC Individual and Company validation', () => {
         // Assert
         expect(results).not.contains(true);
     });
+
+    it('Must be invalid when the value has a sequence', () => {
+        // Arrange
+        const rucList = ['123456789', '12345679', '987654322', '987654322', '98765434', '23456787',
+            '00000000', '11111119', '22222227', '33333335', '44444443', '55555551', '66666660',
+            '77777778', '88888886', '99999994', '000000000', '111111110', '222222220', '333333330',
+            '444444440', '555555550', '666666660', '777777770', '888888880', '999999990'];
+
+        // Act
+        const restuls = rucList.map(ruc => validator.validateRUC(ruc));
+
+        // Assert
+        expect(restuls).not.contains(true);
+    });
 });
